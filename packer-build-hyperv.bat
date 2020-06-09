b230af1465d8449695f8a08a29c2c955254fd696
Guerrero <dan.guerrero@terumobct.com>
2020-06-09 15:48:05 -0600

    changed to use windows_2019.json

--- packer-build-hyperv.bat
+++ packer-build-hyperv.bat
@@ -3,4 +3,4 @@ set PACKER_LOG_PATH=C:\packer-logs\packer-windows\log.txt
 
 del C:\packer-logs\packer-windows\log.txt
 
-packer build -force -only=hyperv-iso -var iso_url=".\iso\17763.737.190906-2324.rs_release_svc_refresh_SERVER_EVAL_x64FRE_en-us_1.iso" -var disk_size=256000 windows_2019-GitHub.json
+packer build -force -only=hyperv-iso -var iso_url=".\iso\17763.737.190906-2324.rs5_release_svc_refresh_SERVER_EVAL_x64FRE_en-us_1.iso" -var disk_size=256000 windows_2019.json
