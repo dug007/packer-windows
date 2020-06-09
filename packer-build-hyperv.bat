62b6e42ce8ac8542bbef885351f9a1a9a2090468
Guerrero <dan.guerrero@terumobct.com>
2020-06-08 20:42:16 -0600

    initial commit

---
+++ packer-build-hyperv.bat
@@ -0,0 +1,6 @@
+set PACKER_LOG=1
+set PACKER_LOG_PATH=C:\packer-logs\packer-windows\log.txt
+
+del C:\packer-logs\packer-windows\log.txt
+
+packer build -force -only=hyperv-iso -var iso_url=".\iso\17763.737.190906-2324.rs_release_svc_refresh_SERVER_EVAL_x64FRE_en-us_1.iso" -var disk_size=256000 windows_2019-GitHub.json
